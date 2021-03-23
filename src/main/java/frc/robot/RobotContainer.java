dff0fc7109f1b16a51b7320dc219ad4e188cb17a
Furiouspuzza <arshan.r@prhsrobotics.com>
2021-03-23 15:44:02 -0400

    added comments
    added comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,20 +5,20 @@ import frc.robot.subsystems.DriveTrain;
 import frc.robot.commands.JoystickDrive;
 
 public class RobotContainer {
-    public final DriveTrain driveTrain  = new DriveTrain();
-    public static XboxController driveController = new XboxController(0);
+    public final DriveTrain driveTrain  = new DriveTrain(); //initializing the drive train
+    public static XboxController driveController = new XboxController(0); //initializing the drive controller on the xbox controller
 
     public RobotContainer() {
 
-        configureButtonBindings();
+        configureButtonBindings(); //setting up the buttons on the xbox controller to the default
 
-        driveTrain.setDefaultCommand(
-            new JoystickDrive(driveTrain)
+        driveTrain.setDefaultCommand( //sets the default command for the drivetrain
+            new JoystickDrive(driveTrain) //plugs the drive train into the joystick drive constructor so that requirements can be added
         );
 
     }
 
-    public void configureButtonBindings() {
-
+    public void configureButtonBindings() { 
+        //sets autonomous path 
     }
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,18 +5,18 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 public class RobotMap{
-    public static final int LEFT_FRONT_PORT = 3;
+    public static final int LEFT_FRONT_PORT = 3;  
     public static final int LEFT_BACK_PORT = 4;
     public static final int RIGHT_FRONT_PORT = 1;
     public static final int RIGHT_BACK_PORT = 2;
+    //assigns port numbers to each port
+    public static WPI_TalonFX leftFrontMotor = new WPI_TalonFX(LEFT_FRONT_PORT); //setting instance of the left front Motor
+    public static WPI_TalonFX rightFrontMotor = new WPI_TalonFX(RIGHT_FRONT_PORT); //setting instance of the right front Motor
+    public static WPI_TalonFX leftBackMotor = new WPI_TalonFX(LEFT_BACK_PORT); //setting instance of the left back Motor
+    public static WPI_TalonFX rightBackMotor = new WPI_TalonFX(RIGHT_BACK_PORT); //setting instance of the right back Motor
 
-    public static WPI_TalonFX leftFrontMotor = new WPI_TalonFX(LEFT_FRONT_PORT);
-    public static WPI_TalonFX rightFrontMotor = new WPI_TalonFX(RIGHT_FRONT_PORT);
-    public static WPI_TalonFX leftBackMotor = new WPI_TalonFX(LEFT_BACK_PORT);
-    public static WPI_TalonFX rightBackMotor = new WPI_TalonFX(RIGHT_BACK_PORT);
-
-    public static PigeonIMU arm_imu = new PigeonIMU(0);
-    public static PigeonIMU drive_imu = new PigeonIMU(1);
+    public static PigeonIMU arm_imu = new PigeonIMU(0); //setting instance of pigeon imu to 0
+    public static PigeonIMU drive_imu = new PigeonIMU(1); //setting instance of pigeon imu to 1
 
     
 
